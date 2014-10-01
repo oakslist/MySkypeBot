15cffeb92b18701cf4b26305a5d9bc0be5d1f4c0
oakslist <oakslist@gmail.com>
2014-10-01 17:45:45 +0300

    added

--- src/main/java/com/oakslist/MyListener.java
+++ src/main/java/com/oakslist/MyListener.java
@@ -27,7 +27,6 @@ public class MyListener implements ChatMessageListener {
         try {
             final Chat chatterup = myChat;
             if (myMessage.toString().equals("getips")) {
-                
                 String currentIp = getIp();
                 System.out.println(currentIp);
                 chatterup.send(currentIp);
